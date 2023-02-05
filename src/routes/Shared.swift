26c3cab9c4a16dfb62f1c30dad18b9daa5ebc021
Gil Shapira <gshapira@gmail.com>
2023-02-05 18:58:24 +0200

    Minor error updates

--- src/routes/Shared.swift
+++ src/routes/Shared.swift
@@ -2,7 +2,7 @@
 extension DescopeClient.JWTResponse {
     func convert() throws -> DescopeSession {
         let sessionToken = try Token(jwt: sessionJwt)
-        guard let refreshJwt else { throw DescopeError.decodeError }
+        guard let refreshJwt else { throw DescopeError.decodeError.with(message: "Missing refresh JWT") }
         let refreshToken = try Token(jwt: refreshJwt)
         return Session(sessionToken: sessionToken, refreshToken: refreshToken)
     }

9d93da45214ec2178643eb4c019fb6408ff16551
Gil Shapira <gshapira@gmail.com>
2023-03-02 18:15:43 +0200

    Minor SDK updates (#29)

--- src/DescopeKit.swift
+++ src/DescopeKit.swift
@@ -10,16 +10,35 @@ public enum Descope {
     public static let name = "DescopeKit"
     
     /// The Descope SDK version
-    public static let version = "1.0.0"
+    public static let version = "0.9.0"
     
-    /// The project ID of your Descope project. You will most likely want to set this
-    /// value during your application's initialization flow.
-    public static var projectId: String = "" {
+    /// The projectId of your Descope project.
+    ///
+    /// You will most likely want to set this value during your application's
+    /// initialization flow, and in most cases you only need to set this to
+    /// work with the SDK.
+    ///
+    /// - Note: This is a shortcut for setting the `config` property.
+    public static var projectId: String {
+        get {
+            return config.projectId
+        }
+        set {
+            precondition(config.projectId == "", "ProjectId should not be set more than once")
+            config = DescopeConfig(projectId: newValue)
+        }
+    }
+    
+    /// The configuration of the Descope SDK.
+    ///
+    /// Set this property instead of `Descope.projectId` in your application's
+    /// initialization flow if you require additional configuration.
+    public static var config: DescopeConfig = .initial {
         willSet {
-            precondition(projectId == "", "ProjectId should not be set more than once")
+            precondition(config.projectId == "", "Config should not be set more than once")
         }
         didSet {
-            precondition(projectId != "", "ProjectId should not be an empty string")
+            precondition(config.projectId != "", "Config should not be an empty string")
         }
     }
     
@@ -48,5 +67,5 @@ public enum Descope {
     public static var sso: DescopeSSO { sdk.sso }
 
     /// Internal SDK object
-    static let sdk = DescopeSDK(projectId: projectId)
+    static let sdk = DescopeSDK(config: config)
 }
--- src/sdk/Config.swift
+++ src/sdk/Config.swift
@@ -1,14 +1,30 @@
 
+/// The configuration of the Descope SDK.
 public struct DescopeConfig {
+    
+    /// The id of the Descope project.
     public var projectId: String
+    
+    /// The base URL of the Descope server.
     public var baseURL: String = "https://api.descope.com"
-    
-    public init(projectId: String) {
+
+    /// Creates a new `DescopeConfig` object.
+    ///
+    /// - Parameters:
+    ///   - projectId The id of the Descope project can be found in the
+    ///   project page in the Descope console
+    ///   - baseURL An optional override for the URL of the Descope server,
+    ///   in case you need to access it through a CNAME record.
+    public init(projectId: String, baseURL: String? = nil) {
         self.projectId = projectId
+        if let baseURL {
+            self.baseURL = baseURL
+        }
     }
-    
-    public init(projectId: String, baseURL: String) {
-        self.projectId = projectId
-        self.baseURL = baseURL
-    }
+}
+
+// Internal
+
+extension DescopeConfig {
+    static let initial = DescopeConfig(projectId: "")
 }

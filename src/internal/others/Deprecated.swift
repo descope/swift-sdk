8325da2ea3f3b448f070d048df702bdc53a8860a
Gil Shapira <gshapira@gmail.com>
2023-11-15 21:59:26 +0200

    Cleanup

--- src/internal/others/Deprecated.swift
+++ src/internal/others/Deprecated.swift
@@ -1,18 +1,6 @@
 
 import Foundation
 
-/// See the documentation for `DescopeUser`.
-@available(*, unavailable, renamed: "DescopeUser")
-public typealias MeResponse = DescopeUser
-
-/// See the documentation for `SignUpDetails`.
-@available(*, unavailable, renamed: "SignUpDetails")
-public typealias User = SignUpDetails
-
-/// See the documentation for `PasswordPolicyResponse`.
-@available(*, unavailable, renamed: "PasswordPolicyResponse")
-public typealias PasswordPolicy = PasswordPolicyResponse
-
 public extension DescopeOTP {
     @available(*, deprecated, message: "Pass a value (or an empty array) for the options parameter")
     func signIn(with method: DeliveryMethod, loginId: String) async throws -> String {

5927e5f089e7d6007ce6edfcefad72f6c9f5dd53
Gil Shapira <gshapira@gmail.com>
2024-01-15 19:44:37 +0200

    Fix compilation issue

--- test/types/User.swift
+++ test/types/User.swift
@@ -3,13 +3,36 @@ import XCTest
 
 class TestUser: XCTestCase {
     func testUserEncoding() throws {
-        let user = DescopeUser(userId: "userId", loginIds: ["loginId"], createdAt: Date(), email: "email", isVerifiedEmail: true, customAttributes: ["a": "yes"])
+        var user = DescopeUser(
+            userId: "userId",
+            loginIds: ["loginId"],
+            createdAt: Date(),
+            email: "email",
+            isVerifiedEmail: true,
+            customAttributes: ["a": "yes"]
+        )
+
         let encodedUser = try JSONEncoder().encode(user)
         let decodedUser = try JSONDecoder().decode(DescopeUser.self, from: encodedUser)
-        XCTAssertEqual(user, decodedUser)
+
         XCTAssertTrue(decodedUser.isVerifiedEmail)
         XCTAssertFalse(decodedUser.isVerifiedPhone)
-        guard let aValue = decodedUser.customAttributes["a"] as? String else { return XCTFail("Couldn't get custom attirubte value as String") }
+        guard let aValue = decodedUser.customAttributes["a"] as? String else { return XCTFail("Couldn't get custom attribute value as String") }
         XCTAssertEqual("yes", aValue)
+
+        XCTAssertEqual(user, decodedUser)
+        XCTAssertTrue(user == decodedUser)
+
+        user.customAttributes["a"] = TestUser()
+        XCTAssertNotEqual(user, decodedUser)
+        XCTAssertTrue(user != decodedUser)
+
+        user.customAttributes["a"] = "no"
+        XCTAssertNotEqual(user, decodedUser)
+        XCTAssertTrue(user != decodedUser)
+
+        user.customAttributes["a"] = "yes"
+        XCTAssertEqual(user, decodedUser)
+        XCTAssertTrue(user == decodedUser)
     }
 }

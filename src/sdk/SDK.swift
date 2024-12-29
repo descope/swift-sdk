1c29aeed9829b1d190e7dde08f97bea37ddbcd8b
Gil Shapira <gshapira@gmail.com>
2024-12-29 14:18:43 +0200

    Bump SDK version

--- src/sdk/SDK.swift
+++ src/sdk/SDK.swift
@@ -143,7 +143,7 @@ public extension DescopeSDK {
     static let name = "DescopeKit"
     
     /// The Descope SDK version
-    static let version = "0.9.8"
+    static let version = "0.9.9"
 }
 
 // Internal
